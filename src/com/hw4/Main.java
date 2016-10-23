96e3d8cd19776bb415629719caf09f3ce52e9372
Aaron Kolker <akolker3@gmail.com>
2016-10-23 16:40:51 -0400

    Set up workspace

---
+++ src/com/hw4/Main.java
@@ -0,0 +1,12 @@
+package com.hw4;
+
+/**
+ * Created by aaron on 10/23/2016.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.print("AYYY LMAO");
+    }
+
+}
